3a8cf30871ccc43b10c9c73b2f6b3f98fe926424
James Hart <hjhart@gmail.com>
2014-06-28 10:39:32 -0700

    readme, NOW

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,9 @@
 name             'php-smartos'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'hjhart'
+maintainer_email 'hjhart@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures php-smartos'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+supports 'smartos'
